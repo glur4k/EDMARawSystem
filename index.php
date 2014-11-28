fe974ebacce3592bb55a365fc773ddcaef2ffb22
Glur4k <tonon.san@gmail.com>
2014-11-28 21:08:56 +0100

    Update

--- index.php
+++ index.php
@@ -5,22 +5,20 @@ if (Session::exists('home')) {
     echo Session::flash('home');
 }
 
-$user = new User();
-if ($user->isLoggedIn()) {
+$projekt = new Projekt();
+if ($projekt->isLoggedIn()) {
 ?>
-    <p>Hi <a href="profile.php?user=<?php echo escape($user->data()->username); ?>"><?php echo escape($user->data()->username); ?></a></p> 
+    <p>Projekt: <?php echo escape($projekt->data()->name); ?></p> 
     
     <ul>
         <li><a href="logout.php">Ausloggen</a></li>
         <li><a href="update.php">Profildaten aktualisieren</a></li>
-        <li><a href="changepassword.php">Passwort ändern</a></li>
     </ul>
 <?php
-    if ($user->hasPermission('admin')) {
+    if ($projekt->isMaster()) {
         echo 'Du bist ein Admin';
     }
 } else {
-?>
-    <p>Bitte anmelden! <a href="login.php">zum Login</a></p>
-<?php
+    Session::flash('login', 'Bitte zu erst anmelden!');
+    Redirect::to('login.php');
 }
